adb583c7771c10e8782d166dff5fd4f51d4b814a
IOSH <iveninvala7@gmal.com>
2024-02-25 13:45:06 +0300

    Init 14: task_service with FastAPI

---
+++ web_application/backend/microservices/task_service/app/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+"""Base config"""
+MODE = os.environ.get("MODE")
+VERSION_API = 1
+NAME_SERVICE = "Task"
+PATH_SERVICE = f"/api/v{VERSION_API}/task"
+ORIGINS = [  # All hosts that can access our api
+    "http://127.0.0.1:8080",
+]
+
+"""Data base"""
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
