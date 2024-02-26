2c9bfc1d8068e76fb494474f14500773302ddff7
IOSH <iveninvala7@gmal.com>
2024-02-26 21:41:52 +0300

    Refactor 17: models and schemas task_service.

--- web_application/backend/microservices/task_service/app/task/model.py
+++ web_application/backend/microservices/task_service/app/task/model.py
@@ -1,6 +1,8 @@
+import datetime
+from typing import Optional
 
-from sqlalchemy import Integer, String, DateTime, func
-from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Integer
+from sqlalchemy.orm import mapped_column, Mapped
 
 from app.db.db import Base
 from app.task.schema import TaskSchema
@@ -9,27 +11,54 @@ from app.task.schema import TaskSchema
 class Task(Base):
     __tablename__ = "task"
 
-    id = mapped_column(Integer, primary_key=True)
-    name = mapped_column(String(255))
-    description = mapped_column(String(255))
+    """
+    Модель, описывающая таблицу `Task`.
+    
+    :param id (int)          - уникальный индификатор `Task`;
+    :param name (str)        - имя `Task`;
+    :param description (str) - описание `Task`;
+    :param create_by (int)   - id пользователя, создавшего эту `Task`;
+    
+    :param id_template (Optional[int])                                    - id шаблона, от которого будут 
+                                                                            наследоваться значения полей `Task`;
+    :param is_complete  (Optional[bool])                                  - завершена ли задача успешно;
+    :param complete_at (Optional[datetime.datatime])                      - когда была выполнена задача;
+    :param planned_complete_at (Optional[datetime.datatime])              - время когда планируется завершить `Task`;
+    :param created_at (Optional[datetime.datetime])                       - когда была создана задача;
+    :param send_notification_at (Optional[datetime.datetime])             - дата и время, в которое будет 
+                                                                            отправлено уведомление о `Task`;
+    :param duration_repeat_send_notification_at (Optional[datetime.time]) - премя за которое надо повторно 
+                                                                            отправить уведомление о `Task`;
+    """
 
-    template = relationship("Template", back_populates="tasks")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    create_by: Mapped[int]
 
-    created_at = mapped_column(DateTime(), server_default=func.CURRENT_TIMESTAMP())
-    created_by = mapped_column()
-
-    send_notification_at = mapped_column(DateTime())
+    id_template: Mapped[Optional[int]]
+    is_complete: Mapped[Optional[bool]]
+    complete_at: Mapped[Optional[datetime.datetime]]
+    planned_complete_at: Mapped[Optional[datetime.datetime]]
+    created_at: Mapped[Optional[datetime.datetime]]
+    send_notification_at: Mapped[Optional[datetime.datetime]]
+    duration_repeat_send_notification_at: Mapped[Optional[datetime.time]]
 
     def __repr__(self) -> str:
         return f"Task(id={self.id!r}, name={self.name!r})"
 
+    """Конвектирование значений таблицы в pydantic model"""
     def to_read_model(self) -> TaskSchema:
         return TaskSchema(
             id=self.id,
             name=self.name,
             description=self.description,
-            template=self.template,
+            create_by=self.create_by,
+            id_template=self.id_template,
+            is_complete=self.is_complete,
+            complete_at=self.complete_at,
+            planned_complete_at=self.planned_complete_at,
             created_at=self.created_at,
-            created_by=self.created_by,
             send_notification_at=self.send_notification_at,
+            duration_repeat_send_notification_at=self.duration_repeat_send_notification_at,
         )
--- web_application/backend/microservices/task_service/app/task/schema.py
+++ web_application/backend/microservices/task_service/app/task/schema.py
@@ -1,31 +1,41 @@
-from datetime import datetime
+import datetime
 from typing import Optional
 
 from pydantic import BaseModel
-from pydantic.v1 import validator
+from pydantic.v1 import validator, constr, PositiveInt
 
 
 class TaskBase(BaseModel):
-    name: str
-    description: Optional[str]
-    created_by: str
-    created_at: datetime
-    send_notification_at: datetime
+    name: constr(min_length=1, max_length=64)
+    description: constr(min_length=0, max_length=255)
+    create_by: PositiveInt
+    is_complete: Optional[bool]
+    complete_at: Optional[datetime.datetime]
+    planned_complete_at: Optional[datetime.datetime]
+    created_at: Optional[datetime.datetime]
+    send_notification_at: Optional[datetime.datetime]
+    duration_repeat_send_notification_at: Optional[datetime.time]
 
-    @validator('created_at', 'send_notification_at')
-    def validate_datetime(self, v):
-        if v < datetime.now():
+    @validator('complete_at', 'planned_complete_at', 'send_notification_at')
+    def validate_completing(self, v):
+        if v < self.created_at:
+            raise ValueError('Datetime must be in the past')
+        return v
+
+    @validator('created_at')
+    def validate_creating(self, v):
+        if v > datetime.datetime.now():
             raise ValueError('Datetime must be in the future')
         return v
 
 
 class TaskCreateSchema(TaskBase):
-    pass
+    template_id: PositiveInt
 
 
 class TaskSchema(TaskBase):
-    id: int
-    template_id: int
+    id: PositiveInt
+    template_id: PositiveInt
 
     class Config:
         orm_mode = True
